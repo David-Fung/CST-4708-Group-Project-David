90db3ce1693309890ba5c2227c4209ab483dd660
Wei Wang <github@weiwang.dev>
2020-04-09 01:32:37 -0400

    Merged Files

---
+++ Group Project/Group Project/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Group_Project
+{
+    internal class Global
+    {
+        private readonly static String _CONN_STRING =
+            "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + "" +
+            "|DataDirectory|\\Comics.mdf" + 
+            ";Integrated Security=True;Connect Timeout=30";
+
+        public static String CONN_STRING
+        {
+            get { return _CONN_STRING; }
+        }
+    }
+}
